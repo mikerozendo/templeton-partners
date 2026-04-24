4132eb6a17c18419e9905757e6b364f5c463a8e2
mikerozendo <mikerozendo@gmail.com>
2026-04-24 19:38:46 -0300

    removing loop from hostedSerivce, since it may run only once

--- src/Templeton.Partners.Api/Features/Stories/FetchBestStories/FetchBestStoriesServiceBackgroundService.cs
+++ src/Templeton.Partners.Api/Features/Stories/FetchBestStories/FetchBestStoriesServiceBackgroundService.cs
@@ -5,13 +5,8 @@ public sealed class FetchBestStoriesServiceBackgroundService(IServiceProvider se
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            using var scope = serviceProvider.CreateScope();
-            var service = scope.ServiceProvider.GetRequiredService<IFetchBestStoriesService>();
-            await service.ExecuteAsync();
-
-            await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
-        }
+        using var scope = serviceProvider.CreateScope();
+        var service = scope.ServiceProvider.GetRequiredService<IFetchBestStoriesService>();
+        await service.ExecuteAsync();
     }
 }
