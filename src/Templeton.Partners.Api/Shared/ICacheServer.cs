f8b4e2378799e593c10c6dd609f8631a3a864a52
mikerozendo <mikerozendo@gmail.com>
2026-04-25 13:08:11 -0300

    fixing cacheserver generics config

--- src/Templeton.Partners.Api/Shared/ICacheServer.cs
+++ src/Templeton.Partners.Api/Shared/ICacheServer.cs
@@ -2,9 +2,9 @@ using Templeton.Partners.Api.Entities;
 
 namespace Templeton.Partners.Api.Shared;
 
-public interface ICacheServer<TEntry>
-    where TEntry : EntryBase
+public interface ICacheServer<TEntry, TData>
+    where TEntry : EntryBase<TData>
 {
-    Task<TEntry?> GetByKeyAsync(string key);
+    Task<TData?> GetByKeyAsync(string key);
     Task SaveAsync(TEntry entry);
 }
