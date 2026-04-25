2d64939716b094dc8fb673fe31daa75d4a1be265
mikerozendo <mikerozendo@gmail.com>
2026-04-25 13:10:06 -0300

    fixing `BestStoriesProvider` to correctly inject ICacheServer

--- src/Templeton.Partners.Api/Features/BestStories/BestStoriesProvider/BestStoriesProvider.cs
+++ src/Templeton.Partners.Api/Features/BestStories/BestStoriesProvider/BestStoriesProvider.cs
@@ -3,14 +3,9 @@ using Templeton.Partners.Api.Shared;
 
 namespace Templeton.Partners.Api.Features.BestStories.BestStoriesProvider;
 
-public sealed class BestStoriesProvider(ICacheServer<BestStoryPageEntry> cacheServer)
+public sealed class BestStoriesProvider(ICacheServer<BestStoryPageEntry, BestStoryPage> cacheServer)
     : IBestStoriesProvider
 {
-    public async Task<BestStoryPage?> GetByPageAsync(int page)
-    {
-        var cacheEntry = await cacheServer.GetByKeyAsync(new BestStoryPageEntry(page).Key);
-        return cacheEntry is null
-            ? null
-            : new BestStoryPage(page, (IEnumerable<long>)cacheEntry.Data);
-    }
+    public async Task<BestStoryPage?> GetByPageAsync(int page) =>
+        await cacheServer.GetByKeyAsync(new BestStoryPageEntry(page).Key);
 }
